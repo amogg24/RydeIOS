c51128155a3962b8e1fa9f28ff8d002319fa6a7f
Joe Fletcher <joevt@vt.edu>
2016-04-01 00:32:50 -0400

    Added NSAppTransport crap to plist. Cleaned up JSON POST by created a method called post thats generic. Need to test if server receives it. Will look into creating local server but hopefully JSON is wrapped right and being sent correctly. This should be close to being done. (Joe)

--- Ryde/CreateAccountViewController.swift
+++ Ryde/CreateAccountViewController.swift
@@ -35,14 +35,17 @@ class CreateAccountViewController: UIViewController {
         //print permissions, such as public_profile
         print(FBSDKAccessToken.currentAccessToken().permissions)
         
+        // Grab data from FB
         let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "id, name, email"])
         graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
         
+            // Set Name
             self.profileName.text = result.valueForKey("name") as? String
         
             let FBid = result.valueForKey("id") as? String
         
             let url = NSURL(string: "https://graph.facebook.com/\(FBid!)/picture?type=large&return_ssl_resources=1")
+            // Set Image
             self.profileImage.image = UIImage(data: NSData(contentsOfURL: url!)!)
         })
     }
@@ -52,36 +55,78 @@ class CreateAccountViewController: UIViewController {
 
     @IBAction func submitCreateAccount(sender: UIBarButtonItem) {
         
-        let JSONObject: [String : AnyObject] = [
+        let JSONObject: [String : String] = [
             "name" : "Joe Fletcher",
             "phone" : "7034857174"
         ]
         
-        if NSJSONSerialization.isValidJSONObject(JSONObject) {
-            var request: NSMutableURLRequest = NSMutableURLRequest()
-            let url = "http://tendinsights.com/user"
-            
-            var err: NSError?
-            
-            request.URL = NSURL(string: url)
-            request.HTTPMethod = "POST"
-            request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.HTTPBody = NSJSONSerialization.dataWithJSONObject(JSONObject, options:  NSJSONWritingOptions(rawValue:0))
-            
-            NSURLConnection.sendSynchronousRequest(request, queue: NSOperationQueue()) {(response, data, error) -> Void in
-                if error != nil {
-                    
-                    print("error")
-                    
-                } else {
-                    
-                    print(response)
-                    
-                }
-            }
+        // Sends a POST to the specified URL with the JSON conent
+        self.post(JSONObject, url: "http://localhost:4567/login")
+        
+        
+    }
+    
+    // Mark - Generic POST function that takes in a JSON dictinoary and the URL to be POSTed to
+    
+    
+    // SOURCE: http://jamesonquave.com/blog/making-a-post-request-in-swift/
+    func post(params : Dictionary<String, String>, url : String) {
+        
+        print(params)
+        
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        let session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: [])
+        } catch {
+            print(error)
+            request.HTTPBody = nil
         }
         
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            print("Response: \(response)")
+            let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("Body: \(strData)")
+            
+            let json: NSDictionary?
+            
+            do {
+                json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
+            } catch let dataError{
+                
+                // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+                print(dataError)
+                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print("Error could not parse JSON: '\(jsonStr)'")
+                // return or throw?
+                return
+            }
+            
+           
+
+            // The JSONObjectWithData constructor didn't return an error. But, we should still
+            // check and make sure that json has a value using optional binding.
+            if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    let success = parseJSON["success"] as? Int
+                    print("Succes: \(success)")
+            }
+            else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print("Error could not parse JSON: \(jsonStr)")
+            }
+        })
+        
+        task.resume()
     }
     
 
