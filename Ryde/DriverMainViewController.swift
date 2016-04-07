e95eb29a838bad6ad9315a0e6b4b6362596027f6
Blake Duncan <blakeduncan@blakes-macbook-pro-4.local>
2016-04-06 20:38:37 -0400

    Driver front end update

---
+++ Ryde/DriverMainViewController.swift
@@ -0,0 +1,77 @@
+//
+//  DriverMainViewController.swift
+//  Ryde
+//
+//  Created by Blake Duncan on 4/6/16.
+//  Copyright © 2016 Jared Deiner. All rights reserved.
+//
+
+import UIKit
+
+class DriverMainViewController: UIViewController {
+    
+    var driverName = "Blake Duncan"
+    var startTime = "10:00 p.m."
+    var endTime = "1:00 a.m."
+    var queueSize = 0
+    
+    @IBOutlet var startLabel: UILabel!
+    @IBOutlet var endLabel: UILabel!
+    @IBOutlet var queueLabel: UILabel!
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var acceptRideButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headerLabel.text = "Welcome " + driverName
+        startLabel.text = "Start Time: " + startTime
+        endLabel.text = "End Time: " + endTime
+        queueLabel.text = "Queue Size: " + String(queueSize)
+        
+        //hide back button
+        self.navigationItem.setHidesBackButton(true, animated:true);
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func acceptRidePressed(sender: UIButton) {
+        performSegueWithIdentifier("AcceptRide", sender: self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    -------------------------
+    MARK: - Prepare for Segue
+    -------------------------
+    */
+    // This method is called by the system whenever you invoke the method performSegueWithIdentifier:sender:
+    // You never call this method. It is invoked by the system.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        
+        if segue.identifier == "AcceptRide" {
+            
+            // Obtain the object reference of the destination (downstream) view controller
+            //let driverMapViewController: DriverMapViewController = segue.destinationViewController as! DriverMapViewController
+            
+            
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ Ryde/DriverMapViewController.swift
@@ -0,0 +1,178 @@
+//
+//  DriverMapViewController.swift
+//  Ryde
+//
+//  Created by Blake Duncan on 4/6/16.
+//  Copyright © 2016 Jared Deiner. All rights reserved.
+//
+
+import UIKit
+
+class DriverMapViewController: UIViewController, UIWebViewDelegate {
+    
+    //37.231200, -80.4102
+    //37.234600, -80.4049
+    //37.241660, -80.418267
+    
+    var riderName = "Blake Duncan"
+    var addressOne = ""
+    var addressTwo = ""
+    var riderLat = 37.234600
+    var riderLng = -80.4102
+    var driverLat = 37.231200
+    var driverLng = -80.4104
+    var destLat = 37.241660
+    var destLng = -80.418267
+    
+    //False if driver hasn't picked up rider and true otherwise
+    var hasRider = false
+    
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var riderNameLabel: UILabel!
+    @IBOutlet var driverButton: UIButton!
+    @IBOutlet var webView: UIWebView!
+    
+    //google maps fields
+    var mapsHtmlFilePath: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //hide back button
+        self.navigationItem.setHidesBackButton(true, animated:true);
+        
+        riderNameLabel.text = riderName
+        hasRider = false
+        
+        mapsHtmlFilePath = NSBundle.mainBundle().pathForResource("maps", ofType: "html")
+        
+        let tempDriverLat = String(driverLat)
+        let tempDriverLng = String(driverLng)
+        let driverCoord = "\(tempDriverLat),\(tempDriverLng)"
+        
+        let tempRiderLat = String(riderLat)
+        let tempRiderLng = String(riderLng)
+        let riderCoord = "\(tempRiderLat),\(tempRiderLng)"
+        
+        let googleMapQuery: String = mapsHtmlFilePath! + "?start=\(driverCoord)&end=\(riderCoord)&traveltype=DRIVING"
+        
+        // Obtain the data passed from the upstream view controller VTPlaceInfoViewController
+        let mapQuery = googleMapQuery
+        
+        /*
+        Convert the mapQuery into an NSURL object and store its object reference
+        into the local variable url. An NSURL object represents a URL.
+        */
+        let url: NSURL? = NSURL(string: mapQuery)
+        
+        /*
+        Convert the NSURL object into an NSURLRequest object and store its object
+        reference into the local variable request. An NSURLRequest object represents
+        a URL load request in a manner independent of protocol and URL scheme.
+        */
+        let request = NSURLRequest(URL: url!)
+        
+        // Ask the webView object to display the web page for the given URL
+        webView.loadRequest(request)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func pickupConfirmed(){
+        headerLabel.text = "Drop Off"
+        driverButton.setTitle("Ride Over", forState: .Normal)
+        hasRider = true
+        
+        //load new directions
+        let tempDriverLat = String(riderLat)
+        let tempDriverLng = String(riderLng)
+        let driverCoord = "\(tempDriverLat),\(tempDriverLng)"
+        
+        let tempDropLat = String(destLat)
+        let tempDropLng = String(destLng)
+        let dropOffCoord = "\(tempDropLat),\(tempDropLng)"
+        
+        let googleMapQuery: String = mapsHtmlFilePath! + "?start=\(driverCoord)&end=\(dropOffCoord)&traveltype=DRIVING"
+        
+        let mapQuery = googleMapQuery
+        
+        let url: NSURL? = NSURL(string: mapQuery)
+        
+        let request = NSURLRequest(URL: url!)
+        webView.loadRequest(request)
+    }
+    
+    @IBAction func pickupButtonPressed(sender: UIButton) {
+        if !hasRider {
+            
+            //confirm pickup
+            let alertController = UIAlertController(title: "Confirm",
+                message: "Confirm that pickup has been made",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            // Create a UIAlertAction object and add it to the alert controller
+            alertController.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action: UIAlertAction!) in
+                self.pickupConfirmed()
+            }))
+            alertController.addAction(UIAlertAction(title: "No", style: .Default, handler: nil))
+            
+            // Present the alert controller by calling the presentViewController method
+            presentViewController(alertController, animated: true, completion: nil)
+            
+        } else if hasRider{
+            
+            //confirm ride over and drop off made
+            let alertController = UIAlertController(title: "Confirm",
+                message: "Confirm that ride is over and drop off has been made",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            // Create a UIAlertAction object and add it to the alert controller
+            alertController.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action: UIAlertAction!) in
+                self.navigationController?.popViewControllerAnimated(true)
+            }))
+            alertController.addAction(UIAlertAction(title: "No", style: .Default, handler: nil))
+            
+            // Present the alert controller by calling the presentViewController method
+            presentViewController(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func cancelPickupPressed(sender: UIBarButtonItem) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    /*
+    -------------------------
+    MARK: - Prepare for Segue
+    -------------------------
+    */
+    // This method is called by the system whenever you invoke the method performSegueWithIdentifier:sender:
+    // You never call this method. It is invoked by the system.
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        
+        if segue.identifier == "RideOver" {
+            
+            // Obtain the object reference of the destination (downstream) view controller
+            //let driverMapViewController: DriverMapViewController = segue.destinationViewController as! DriverMapViewController
+            
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
