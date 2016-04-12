c4dc82c2934695d5a4950f9aa0b5cc75b5e11a55
Cody Cummings <Cody@codys-mac-mini.local>
2016-04-12 10:54:31 -0400

    got rid of method in login view controller

--- Ryde/ViewController.swift
+++ Ryde/ViewController.swift
@@ -59,9 +59,7 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
                 print("home")
                 
                 appDelegate.FBid = FBSDKAccessToken.currentAccessToken().userID
-                
-                getAccountInformation()
-                
+                                
                 performSegueWithIdentifier("Home", sender: self)
                 
             }
@@ -118,78 +116,6 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         }
         
         task.resume()
-    }
-    
-    func getAccountInformation() {
-        print("RETRIEVE USERS INFORMATION")
-        
-        let searchTextNoSpaces = searchText.stringByReplacingOccurrencesOfString(" ", withString: "+")
-        
-        let url = NSURL(string: "http://\(self.appDelegate.baseURL)/Ryde/api/user/name/\(searchTextNoSpaces)")
-        
-        print(url)
-        
-        // Creaste URL Request
-        let request = NSMutableURLRequest(URL:url!);
-        
-        // Set request HTTP method to GET. It could be POST as well
-        request.HTTPMethod = "GET"
-        
-        // If needed you could add Authorization header value
-        //request.addValue("Token token=884288bae150b9f2f68d8dc3a932071d", forHTTPHeaderField: "Authorization")
-        
-        // Execute HTTP Request
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            
-            // Check for error
-            if error != nil
-            {
-                print("error=\(error)")
-                return
-            }
-            
-            // Print out response string
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            //print("responseString = \(responseString!)")
-            
-            
-            let json: [NSDictionary]?
-            
-            do {
-                
-                json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? [NSDictionary]
-                
-            } catch let dataError{
-                
-                // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-                print(dataError)
-                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                print("Error could not parse JSON: '\(jsonStr!)'")
-                // return or throw?
-                return
-            }
-            
-            // The JSONObjectWithData constructor didn't return an error. But, we should still
-            // check and make sure that json has a value using optional binding.
-            if let parseJSON = json {
-                // Okay, the parsedJSON is here, lets store its values into an array
-                self.searchBarResults = parseJSON as [NSDictionary]
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.groupMemberTableView.reloadData()
-                })
-            }
-            else {
-                // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                print("Error could not parse JSON: \(jsonStr!)")
-            }
-            
-            
-        })
-        
-        task.resume()
-
     }
     
     
