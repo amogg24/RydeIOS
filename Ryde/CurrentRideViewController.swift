3d0263ed01b34180703a40f6d9febc1d17dbdcde
Franki Yeung <ftyyeung@vt.edu>
2016-04-19 10:37:58 -0400

    Changed all rider to work with jupiter.

--- Ryde/CurrentRideViewController.swift
+++ Ryde/CurrentRideViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class CurrentRideViewController: UIViewController {
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+    // Rider FB id
+    var FBid = ""
     
     override func viewDidLoad() {
         // gets rid of back button in navigation
         let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.Plain, target: navigationController, action: nil)
+        
+        // Grab data from FB
+        let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "id, name, email"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            self.FBid = (result.valueForKey("id") as? String)!
+        })
         
         self.title = "Current Ride"
         
@@ -41,6 +54,10 @@ class CurrentRideViewController: UIViewController {
         
         alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action: UIAlertAction!) in
             self.tabBarController?.tabBar.hidden = false
+            
+            let postUrl = ("http://\(self.appDelegate.baseURL)/Ryde/api/ride/cancel/" + self.FBid)
+            self.postCancel(postUrl)
+            
             self.navigationController?.popToRootViewControllerAnimated(true)
         }))
         
@@ -49,6 +66,28 @@ class CurrentRideViewController: UIViewController {
         }))
         
         presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    // SOURCE: http://jamesonquave.com/blog/making-a-post-request-in-swift/
+    // Post Function for Canceling
+    func postCancel(url : String) {
+        
+        //let params: [String : AnyObject] = [:]
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        let session = NSURLSession.sharedSession()
+        request.HTTPMethod = "DELETE"
+        
+        let task = session.dataTaskWithRequest(request)
+        {
+            (data, response, error) in
+            guard let _ = data else {
+                print("error calling")
+                return
+            }
+            print("canceling")
+        }
+        
+        task.resume()
     }
     
     /*
