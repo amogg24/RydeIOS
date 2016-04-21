e06f4ba12cebc65045ee24474f0c123febfea89d
Franki Yeung <ftyyeung@vt.edu>
2016-04-20 23:55:16 -0400

    Added side bar, and removed cancelling ride button on ride

--- Ryde/CurrentRideViewController.swift
+++ Ryde/CurrentRideViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
 
-class CurrentRideViewController: UIViewController {
+class CurrentRideViewController: UIViewController, RiderSlideMenuDelegate {
     
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
@@ -19,7 +19,7 @@ class CurrentRideViewController: UIViewController {
     
     override func viewDidLoad() {
         // gets rid of back button in navigation
-        let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.Plain, target: navigationController, action: nil)
+        //let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.Plain, target: navigationController, action: nil)
         
         // Grab data from FB
         let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "id, name, email"])
@@ -28,8 +28,8 @@ class CurrentRideViewController: UIViewController {
         })
         
         self.title = "Current Ride"
-        
-        navigationItem.leftBarButtonItem = backButton
+        self.addSlideMenuButton()
+        //navigationItem.leftBarButtonItem = backButton
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
@@ -38,11 +38,6 @@ class CurrentRideViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    
-    @IBAction func cancelRideClicked(sender: UIButton) {
-        cancelRideAlert()
     }
     
     /*
@@ -88,6 +83,112 @@ class CurrentRideViewController: UIViewController {
         }
         
         task.resume()
+    }
+    
+    func slideMenuItemSelectedAtIndex(index: Int32) {
+        let topViewController : UIViewController = self.navigationController!.topViewController!
+        print("View Controller is : \(topViewController) \n", terminator: "")
+        switch(index){
+        case 0:
+            print("Contact Driver\n", terminator: "")
+            
+            cancelRideAlert()
+            
+            break
+        case 1:
+            print("Cancel Ride\n", terminator: "")
+            
+            cancelRideAlert()
+            
+            break
+        default:
+            print("default\n", terminator: "")
+        }
+    }
+    
+    func openViewControllerBasedOnIdentifier(strIdentifier:String){
+        let destViewController : UIViewController = self.storyboard!.instantiateViewControllerWithIdentifier(strIdentifier)
+        
+        let topViewController : UIViewController = self.navigationController!.topViewController!
+        
+        if (topViewController.restorationIdentifier! == destViewController.restorationIdentifier!){
+            print("Same VC")
+        } else {
+            self.navigationController!.pushViewController(destViewController, animated: true)
+        }
+    }
+    
+    func addSlideMenuButton(){
+        let btnShowMenu = UIButton(type: UIButtonType.System)
+        btnShowMenu.setImage(self.defaultMenuImage(), forState: UIControlState.Normal)
+        btnShowMenu.frame = CGRectMake(0, 0, 30, 30)
+        btnShowMenu.addTarget(self, action: #selector(self.onSlideMenuButtonPressed(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        let customBarItem = UIBarButtonItem(customView: btnShowMenu)
+        self.navigationItem.leftBarButtonItem = customBarItem;
+    }
+    
+    func defaultMenuImage() -> UIImage {
+        var defaultMenuImage = UIImage()
+        
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(30, 22), false, 0.0)
+        
+        UIColor.blackColor().setFill()
+        UIBezierPath(rect: CGRectMake(0, 3, 30, 1)).fill()
+        UIBezierPath(rect: CGRectMake(0, 10, 30, 1)).fill()
+        UIBezierPath(rect: CGRectMake(0, 17, 30, 1)).fill()
+        
+        UIColor.whiteColor().setFill()
+        UIBezierPath(rect: CGRectMake(0, 4, 30, 1)).fill()
+        UIBezierPath(rect: CGRectMake(0, 11,  30, 1)).fill()
+        UIBezierPath(rect: CGRectMake(0, 18, 30, 1)).fill()
+        
+        defaultMenuImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return defaultMenuImage;
+    }
+    
+    func onSlideMenuButtonPressed(sender : UIButton){
+        if (sender.tag == 10)
+        {
+            // To Hide Menu If it already there
+            self.slideMenuItemSelectedAtIndex(-1);
+            
+            sender.tag = 0;
+            
+            let viewMenuBack : UIView = view.subviews.last!
+            
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                var frameMenu : CGRect = viewMenuBack.frame
+                frameMenu.origin.x = -1 * UIScreen.mainScreen().bounds.size.width
+                viewMenuBack.frame = frameMenu
+                viewMenuBack.layoutIfNeeded()
+                viewMenuBack.backgroundColor = UIColor.clearColor()
+                }, completion: { (finished) -> Void in
+                    viewMenuBack.removeFromSuperview()
+            })
+            
+            return
+        }
+        
+        sender.enabled = false
+        sender.tag = 10
+        
+        let menuVC : RiderMenuViewController = self.storyboard!.instantiateViewControllerWithIdentifier("RiderMenuViewController") as! RiderMenuViewController
+        menuVC.btnMenu = sender
+        menuVC.delegate = self
+        self.view.addSubview(menuVC.view)
+        self.addChildViewController(menuVC)
+        menuVC.view.layoutIfNeeded()
+        
+        
+        menuVC.view.frame=CGRectMake(0 - UIScreen.mainScreen().bounds.size.width, 0, UIScreen.mainScreen().bounds.size.width, UIScreen.mainScreen().bounds.size.height);
+        
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            menuVC.view.frame=CGRectMake(0, 0, UIScreen.mainScreen().bounds.size.width, UIScreen.mainScreen().bounds.size.height);
+            sender.enabled = true
+            }, completion:nil)
     }
     
     /*
--- Ryde/RiderRequestGroupTableViewController.swift
+++ Ryde/RiderRequestGroupTableViewController.swift
@@ -266,39 +266,32 @@ class RiderRequestGroupTableViewController: UITableViewController {
     // SOURCE: http://jamesonquave.com/blog/making-a-post-request-in-swift/
     func postTAD(params : Dictionary<String, String>, url : String) {
         
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        let session = NSURLSession.sharedSession()
-        request.HTTPMethod = "POST"
-        
-        do {
-            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: [])
-        } catch {
-            print(error)
-            request.HTTPBody = nil
-        }
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            print("Response: \(response)")
-            let strData = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            print("Body: \(strData)")
-            
+         //let params: [String : AnyObject] = [:]
+         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+         let session = NSURLSession.sharedSession()
+         request.HTTPMethod = "POST"
+         
+         let task = session.dataTaskWithRequest(request)
+         {
+            (data, response, error) in
+            guard let _ = data else {
+                print("error calling")
+                return
+            }
+         
             let json: NSDictionary?
-            
+         
             do {
                 json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
             } catch let dataError{
-                
+         
                 // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
                 print(dataError)
                 let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
                 print("Error could not parse JSON: '\(jsonStr)'")
-                self.passcodeError()
                 return
             }
-
+         
             // The JSONObjectWithData constructor didn't return an error. But, we should still
             // check and make sure that json has a value using optional binding.
             if let parseJSON = json {
@@ -320,8 +313,9 @@ class RiderRequestGroupTableViewController: UITableViewController {
                 let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
                 print("Error could not parse JSON: \(jsonStr)")
             }
-        })
-        
+            print("canceling")
+        }
+         
         task.resume()
     }
     
@@ -330,47 +324,6 @@ class RiderRequestGroupTableViewController: UITableViewController {
     // Post Function for request
     func postRequest(params : Dictionary<String, AnyObject>, url : String) {
         
-        //let params: [String : AnyObject] = [:]
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        let session = NSURLSession.sharedSession()
-        request.HTTPMethod = "POST"
-        
-        let task = session.dataTaskWithRequest(request)
-        {
-            (data, response, error) in
-            guard let _ = data else {
-                print("error calling")
-                return
-            }
-            
-            let json: NSDictionary?
-            
-            do {
-                json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableLeaves) as? NSDictionary
-            } catch let dataError{
-                
-                // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
-                print(dataError)
-                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                print("Error could not parse JSON: '\(jsonStr)'")
-                return
-            }
-            
-            // The JSONObjectWithData constructor didn't return an error. But, we should still
-            // check and make sure that json has a value using optional binding.
-            if let parseJSON = json {
-                self.queuePos = (parseJSON["position"] as? Int)!
-            }
-            else {
-                // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
-                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                print("Error could not parse JSON: \(jsonStr)")
-            }
-            print("canceling")
-        }
-        
-        task.resume()
-        /*
         let request = NSMutableURLRequest(URL: NSURL(string: url)!)
         let session = NSURLSession.sharedSession()
         request.HTTPMethod = "POST"
@@ -415,7 +368,7 @@ class RiderRequestGroupTableViewController: UITableViewController {
             }
         })
         
-        task.resume()*/
+        task.resume()
     }
     
     /*
